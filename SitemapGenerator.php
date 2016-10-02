584c87e37ee0ab5b3e60708332d1ea36bf90bced
Nick Peirson <nickpeirson@gmail.com>
2016-10-02 20:33:30 +0100

    Mostly working, just need to format sitemap data to XML

--- SitemapGenerator.php
+++ SitemapGenerator.php
@@ -1,8 +1,12 @@
 <?php
 namespace Nickpeirson\Sculpin\Bundle\SitemapBundle;
 
+use Dflydev\DotAccessConfiguration\Configuration;
 use Sculpin\Core\Event\SourceSetEvent;
+use Sculpin\Core\Permalink\Permalink;
 use Sculpin\Core\Sculpin;
+use Sculpin\Core\Source\AbstractSource;
+use Sculpin\Core\Source\MemorySource;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class SitemapGenerator implements EventSubscriberInterface
@@ -14,7 +18,7 @@ class SitemapGenerator implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            Sculpin::EVENT_BEFORE_RUN => 'beforeRun',
+            Sculpin::EVENT_AFTER_FORMAT => 'afterFormat',
         );
     }
 
@@ -23,11 +27,46 @@ class SitemapGenerator implements EventSubscriberInterface
      *
      * @param SourceSetEvent $sourceSetEvent Source Set Event
      */
-    public function beforeRun(SourceSetEvent $sourceSetEvent)
+    public function afterFormat(SourceSetEvent $sourceSetEvent)
     {
-        /** @var SourceInterface $source */
+        $sitemap = [];
+        /** @var AbstractSource $source */
         foreach ($sourceSetEvent->allSources() as $source) {
-            var_dump($source->data()->export());
+            $data = $source->data()->export();
+            if (empty($data) || !$source->canBeFormatted()) {
+                continue;
+            }
+            $loc = $data['url'];
+            if (isset($data['canonical'])) {
+                $loc = $data['canonical'];
+            }
+            if (is_callable([$source, 'file'])) {
+                $lastmod = date(DATE_W3C, $source->file()->getMTime());
+            } else {
+                $lastmod = date(DATE_W3C);
+            }
+            $url = [
+                'loc' => $loc,
+                'lastmod' => $lastmod
+            ];
+            if (isset($data['sitemap'])) {
+                $url = array_merge($url, $data['sitemap']);
+            }
+            $sitemap[$url['loc']] = $url;
         }
+        $sitemapSource = new MemorySource(
+            'Sitemap',
+            new Configuration,
+            'sitemap',
+            print_r($sitemap, true),
+            'sitemap.xml',
+            'sitemap.xml',
+            null,
+            false,
+            false,
+            true
+        );
+        $sitemapSource->setPermalink(new Permalink('sitemap.xml', 'sitemap.xml'));
+        $sourceSetEvent->sourceSet()->mergeSource($sitemapSource);
     }
 }
